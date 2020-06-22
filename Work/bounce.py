3d5c00fefa5cb647b0b6917a000e115a3474044e
zachbogart <zlbogart@gmail.com>
2020-06-21 20:31:30 -0700

    initial exercises

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,10 @@
 # bounce.py
 #
 # Exercise 1.5
+reduction_value = 0.6
+num_bounces = 10
+height = 100
+
+for bounce in range(1, num_bounces + 1):
+    height = height * reduction_value
+    print(bounce, round(height, 4))
